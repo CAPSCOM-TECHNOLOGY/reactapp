e069b9d7c99113ffd2be64bd6901d50866aa5536
Hemant singh <capscomtech@gmail.com>
2021-01-16 13:01:58 +0530

    lorem para added to app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App(){
       <p className="center">
         The computer educational institute.
       </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum saepe maiores molestiae nulla praesentium voluptas harum illo culpa, porro autem beatae eos quaerat nihil eius fugiat quas! Ex, doloribus a.
+      </p>
     </>
   )
 }
